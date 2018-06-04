f725dc69b6b61cbfda00cb64960aa615daf78234
EC2 Default User <miyu.kawabata@rakuten.com>
2018-06-04 01:00:04 +0000

    add columns

--- resources/views/Tasks/index.blade.php
+++ resources/views/Tasks/index.blade.php
@@ -2,16 +2,27 @@
 
 @section('content')
 
-    <h1>タスク一覧</h1>
+    <h1>TaskList</h1>
 
-    @if (count($tasks) > 0)
-        <ul>
-            @foreach ($tasks as $task)
-                <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->status}} > {{ $task->content }}</li>
-            @endforeach
-        </ul>
+   @if (count($tasks) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>Status</th>
+                    <th>ToDoList</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tasks as $task)
+                    <tr>
+                        <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+                        <td>{{ $task->status }}</td>
+                        <td>{{ $task->content }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     @endif
-    
-    {!! link_to_route('tasks.create', 'タスクメッセージの投稿') !!}
-
+     {!! link_to_route('tasks.create', 'NewTask投稿', null, ['class' => 'btn btn-primary']) !!}
 @endsection
